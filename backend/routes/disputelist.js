b64c04228d961259dee3f0a25954de0b4fb70389
Diana Jill <daajill71@gmail.com>
2023-08-25 13:01:34 +0300

    edited disputelist routes Aug2523

--- backend/routes/disputelist.js
+++ backend/routes/disputelist.js
@@ -4,18 +4,34 @@ const express = require('express');
 const router = express.Router();
 const Dispute = require('../models/Dispute'); // Import your MongoDB model for disputes
 
-// Define a route to get a list of disputes
-router.get('/', async (req, res) => {
-  try {
-    // Fetch the list of disputes from the database
-    const disputes = await Dispute.find({}, 'clientName description');
 
-    // Respond with the list of disputes
-    return res.status(200).json(disputes);
+
+// Route to fetch data
+router.get('/fetch-data', async (req, res) => {
+  try {
+    // Log before retrieving data
+    console.log('Fetching data from MongoDB');
+
+    // Fetch data from MongoDB using the model
+    const data = await YourModel.find();
+
+    // Log after data retrieval
+    console.log('Data retrieved successfully');
+
+    // Send the data as a JSON response
+    res.json(data);
   } catch (error) {
-    console.error('Error fetching disputes:', error);
-    return res.status(500).json({ message: 'Error fetching disputes' });
+    // Handle errors and log them
+    console.error('Error retrieving data from MongoDB:', error);
+
+    // Send an error response
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 module.exports = router;
+
+
+
+
+
